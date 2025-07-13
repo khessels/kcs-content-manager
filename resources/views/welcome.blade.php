c97b0168b3d9de7f3030b49455d792e8bd71e787
khessels <kees.hessels@gmail.com>
2025-07-12 20:54:57 -0600

    added changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Kcs Content Manager</title>
         <link rel="icon" href="/favicon.ico" >
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -68,26 +68,15 @@
                                 id="docs-card"
                                 class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
-                                <div id="screenshot-container" class="relative flex w-full flex-1 items-stretch">
-                                    <img
-                                        src="https://laravel.com/assets/img/welcome/docs-light.svg"
-                                        alt="Laravel documentation screenshot"
-                                        class="aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.06)] dark:hidden"
-                                        onerror="
-                                            document.getElementById('screenshot-container').classList.add('!hidden');
-                                            document.getElementById('docs-card').classList.add('!row-span-1');
-                                            document.getElementById('docs-card-content').classList.add('!flex-row');
-                                            document.getElementById('background').classList.add('!hidden');
-                                        "
-                                    />
-                                    <img
-                                        src="https://laravel.com/assets/img/welcome/docs-dark.svg"
-                                        alt="Laravel documentation screenshot"
-                                        class="hidden aspect-video h-full w-full flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block"
-                                    />
-                                    <div
-                                        class="absolute -bottom-16 -left-16 h-40 w-[calc(100%+8rem)] bg-gradient-to-b from-transparent via-white to-white dark:via-zinc-900 dark:to-zinc-900"
-                                    ></div>
+                                <div id="screenshot-container" class="relative w-full">
+                                    <p>Finally a cms that combines the main features of wordpress with the power of Laravel.</p>
+                                    <p>This system is build to give you internationalisation and content management that works for team development and deployment.</p>
+                                    <br>
+                                    <p>For example, in blade we would use expressions like: {{ __('cms::content.welcome') }} and we (as the programmer) would have to build a corresponding resource file in the correct language.</p>
+                                    <p>So we build a system that lets you use a blade tag like this: <pre>{{!! @c(['editable' => true, 'page' => 'a page or var containing the page name', 'key' => 'main', 'default' => 'some text']) !!}}</pre></p>
+                                    <p>When we are ready with building our pages, we ask the cms module to scan the files, and collect and send the tags to the cloud, where we can manage it.</p>
+
+
                                 </div>
 
                                 <div class="relative flex items-center gap-6 lg:items-end">
